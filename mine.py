ffd84c97058966cdf89fb74185aaff94bd100b6b
kodiankodes <arthuremmanuel270@gmail.com>
2023-10-15 23:14:28 +0100

    Added my Name and Email address to mine.py under My branch Emmanuel-Arthur

--- mine.py
+++ mine.py
@@ -3,5 +3,7 @@ name = "PUT YOUR NAME"
 email = 'PUT YOUR EMAIL'
 
 # Print the name and email address
+name = "Emmanuel Arthur"
+email = "arthuremmanuel270@gmail.com"
 print("Name:", name)
 print("Email:", email)
